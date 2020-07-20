b18a3554a3442a4d91f1662f32d6d7ae270554d5
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-07-20 09:39:43 +0200

    0.3.0 changed folder structure for pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yongshi-pynfc",
-    version="0.2.0",
+    version="0.3.0",
     author="Michael-Yongshi",
     author_email="4registration@outlook.com",
     description="A nfc library for python",
