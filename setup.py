20ccde808511bbd3aa979e0938e1ca80b9e8e1ff
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-07-20 09:10:52 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yongshi-pynfc",
+    version="0.1",
+    author="Michael-Yongshi",
+    author_email="4registration@outlook.com",
+    description="A nfc library for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Michael-Yongshi/Py-Library-NFC",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
