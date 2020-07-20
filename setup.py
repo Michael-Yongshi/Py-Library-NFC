f9549da0ce6bb9efb321cbf7189ab26b0d4d993b
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-07-20 09:21:39 +0200

    bump to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yongshi-pynfc",
-    version="0.1.1",
+    version="0.2.0",
     author="Michael-Yongshi",
     author_email="4registration@outlook.com",
     description="A nfc library for python",
