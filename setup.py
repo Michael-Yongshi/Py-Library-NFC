328683729ef6bc6f911ac9ed1261232b8740f243
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-09-03 09:14:56 +0200

    0.3.8 delete print reference

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yongshi-pynfc",
-    version="0.3.6",
+    version="0.3.8",
     author="Michael-Yongshi",
     author_email="4registration@outlook.com",
     description="A nfc library for python",
