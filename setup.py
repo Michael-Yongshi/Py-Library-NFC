8f0a4d31e777c529990ccff4c318ba1918ada727
Michael-Yongshi <4registration@outlook.com>
2020-12-11 20:55:25 +0100

    bump to 4.2 default new method

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yongshi-pynfc",
-    version="0.4.01",
+    version="0.4.2",
     author="Michael-Yongshi",
     author_email="4registration@outlook.com",
     description="A nfc library for python",
